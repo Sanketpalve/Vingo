ad26f58386333b679bce666e8df2c8655b593096
Sanketpalve <sanketpalve72@gmail.com>
2025-09-15 19:20:22 +0530

    Nav bar updated

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -7,6 +7,7 @@ import { RxCross2 } from "react-icons/rx";
 import { setUserData } from '../redux/userSlice';
 import axios from 'axios'
 import { serverUrl } from '../App.jsx';
+import { FaPlus } from "react-icons/fa";
 
 
 
@@ -24,8 +25,9 @@ function Nav() {
     }
   }
   return (
-    <div className='w-full h-[80px] flex items-center justify-between md:justify-center gap-[30px] px-[20px] fixed top-0 z-[9999] bg-[#fff9f6] overflow-visible'>
-    {showSearch && <div className='w-[90%] h-[50px] bg-white shadow-xl rounded-lg items-center gap-[20px] flex fixed top-[80px] left-[5%]'>
+    <div className='w-full h-[80px] flex items-center justify-between md:justify-center gap-[30px] fixed top-0 z-[9999] bg-[#fff9f6] overflow-visible'>
+
+    {showSearch && userData.role=="user" && <div className='w-[90%] h-[50px] bg-white shadow-xl rounded-lg items-center gap-[20px] flex fixed top-[80px] left-[5%]'>
         <div className='flex items-center w-[30%] overflow-hidden gap-[10px] px-[10px] border-r-[2px] border-gray-400'>
           <FaLocationDot size={25} className=' text-[#ff4d2d]'/>
           <div className='w-[80%] truncate text-gray-600'>{city}</div>
@@ -39,7 +41,7 @@ function Nav() {
 
 
       <h1 className='text-3xl font-bold mb-2 text-[#ff4d2d]'>Vingo</h1>
-       <div className='md:w-[60%] lg:w-40% h-[70px] bg-white shadow-xl rounded-lg items-center gap-[20px] hidden md:flex'>
+      {userData.role=="user" && <div className='md:w-[60%] lg:w-40% h-[70px] bg-white shadow-xl rounded-lg items-center gap-[20px] hidden md:flex'>
         <div className='flex items-center w-[30%] overflow-hidden gap-[10px] px-[10px] border-r-[2px] border-gray-400'>
           <FaLocationDot size={25} className=' text-[#ff4d2d]'/>
           <div className='w-[80%] truncate text-gray-600'>{city}</div>
@@ -48,15 +50,23 @@ function Nav() {
           <IoIosSearch size={25} className=' text-[#ff4d2d]'/>
           <input type='text' placeholder='search delicious food...' className='px-[10px] text-gray-700 outline-0 w-full'/>
         </div>
-       </div>
+       </div>}
+       
        <div className='flex items-center gap-4'>
+        {userData.role=="user" && showSearch?<RxCross2 className=' text-[#ff4d2d] md:hidden' onClick={()=>setShowSearch(false)}/>:<IoIosSearch size={25} className=' text-[#ff4d2d] md:hidden' onClick={()=>setShowSearch(true)}/>}
 
-       {showSearch?<RxCross2 className=' text-[#ff4d2d] md:hidden' onClick={()=>setShowSearch(false)}/>:<IoIosSearch size={25} className=' text-[#ff4d2d] md:hidden' onClick={()=>setShowSearch(true)}/>}
+        {userData.role=="owner" && <>
+            <button className='flex items-center gap-1 p-2 cursor-pointer rounded-full bg-[#ff4d2d]/10 text-[#ff4d2d]'>
+              <FaPlus size={20} />
+              <span>Add Food Item</span>
+            </button>
+        </>}
   
-        <div className='relative cursor-pointer flex items-center'>
+        {userData.role=="user" && <div className='relative cursor-pointer flex items-center'>
           <LuShoppingCart size={25} className='text-[#ff4d2d]'/>
           <span className='absolute right-[-9px] top-[-12px] text-[#ff4d2d]'>0</span>
-        </div>
+        </div>}
+
         <button className='hidden md:block px-3 py-1 rounded-lg bg-[#ff4d2d]/10 text-[#ff4d2d] text-sm font-medium'>
           My Orders 
         </button>
--- frontend/src/components/UserDashboard.jsx
+++ frontend/src/components/UserDashboard.jsx
@@ -3,7 +3,7 @@ import Nav from './Nav'
 
 function UserDashboard() {
   return (
-    <div className="min-h-screen bg-[#fff9f6] flex flex-col">
+    <div className='w-full min-h-screen bg-[#fff9f6] flex flex-col items-center'>
       <Nav/>
     </div>
   )
