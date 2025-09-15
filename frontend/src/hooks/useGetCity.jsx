ad26160ce0e6cddbc66f98b93c8eea8d3f4dc820
Sanketpalve <sanketpalve72@gmail.com>
2025-09-15 16:15:07 +0530

    some errors are fixed

--- frontend/src/hooks/useGetCity.jsx
+++ frontend/src/hooks/useGetCity.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { serverUrl } from '../App'
 import axios from 'axios'
 import { useDispatch, useSelector } from 'react-redux'
-import { setCity, setUserData } from '../redux/userSlice'
+import { setCity, setUserData } from '../redux/userSlice.js'
 
 function useGetCity() {
   const apiKey=import.meta.env.VITE_GEOAPIKEY
@@ -16,7 +16,7 @@ function useGetCity() {
         const result=await axios.get(`https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&format=json&apiKey=${apiKey}`)
         //console.log(result)
         //console.log(result.data.reults[0].city)
-        dispatch(setCity(result?.data.reults[0].city))
+        dispatch(setCity(result?.data.results[0].city))
     })
   },[userData])
 }
