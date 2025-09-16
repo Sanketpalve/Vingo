39054854f2e63f0db95e77c6696f714b3d930cb7
Sanketpalve <sanketpalve72@gmail.com>
2025-09-16 17:35:45 +0530

    crateEditShop

--- frontend/src/components/OwnerDashboard.jsx
+++ frontend/src/components/OwnerDashboard.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import Nav from './Nav'
 import { useSelector } from 'react-redux'
 import { FaUtensils } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
 
 
 function OwnerDashboard() {
+  const navigate = useNavigate()
   const {myShopData}=useSelector(state=>state.owner)
   return (
     <div className='w-full min-h-screen bg-[#fff9f6] flex flex-col items-center'>
@@ -16,8 +18,8 @@ function OwnerDashboard() {
                 <FaUtensils className='text-[#ff4d2d] w-16 h-16 sm:h-20 mb-4'/>
                 <h2 className='text-xl sm:text-2xl font-bold text-gray-800 mb-2'>Add Your Restaurant</h2>
                 <p className='text-gray-600 mb-4 text-sm sm:text-base'>Join our food delivery platform and reach thousands of hungry customers every day</p>
-                <button className='bg-[#ff4d2d] text-white px-5 sm:px-6 py-2 rounded-full font-medium shadow-md hover:bg-orange-200 transition-colors duration-200'>Get started</button>
-                
+                <button className='bg-[#ff4d2d] text-white px-5 sm:px-6 py-2 rounded-full font-medium shadow-md hover:bg-orange-200 transition-colors duration-200' onClick={()=>navigate("/create-edit-shop")}>Get started</button>
+
               </div>
           </div>
         </div>
