59d3a5b59cd50024c2d77ce884bdfa7182197524
Sanketpalve <sanketpalve72@gmail.com>
2025-09-17 22:28:55 +0530

    Owner Item Card

--- backend/controllers/shop.controller.js
+++ backend/controllers/shop.controller.js
@@ -28,7 +28,10 @@ export const createEditShop=async (req,res) => {
 
 export const getMyShop=async (req,res) => {
     try {
-        const shop=await Shop.findOne({owner:req.userId}).populate("owner")
+        const shop=await Shop.findOne({owner:req.userId}).populate("owner").populate({
+            path:"items",
+            options:{sort:{updatedAt:-1}}
+        })
         if(!shop){
             return null
         }
