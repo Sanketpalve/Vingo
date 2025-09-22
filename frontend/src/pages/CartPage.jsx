d4ff0ba6c0933c9bc94500b7b5d7cc609ca95347
Sanketpalve <sanketpalve72@gmail.com>
2025-09-22 16:17:15 +0530

    Checkout feature

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -34,7 +34,7 @@ function CartPage() {
                 <span className='text-xl font-bold text-[#ff4d2d]'>₹{totalAmount}</span>
             </div>
             <div className='mt-4 flex justify-end'>
-                <button className='bg-[#ff4d2d] text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-[#e64526] transition cursor-pointer'>Proceed to CheckOut</button>
+                <button className='bg-[#ff4d2d] text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-[#e64526] transition cursor-pointer'onClick={()=>navigate("/checkout")}>Proceed to CheckOut</button>
             </div>
             </>
         )}
